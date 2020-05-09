edf359045dd7a73c07770c90ab2109aa43b9c235
Mario Rivera Olivo <mario_rivera.olivo@outlook.es>
2020-05-08 19:48:22 -0600

    Created gen_pwd.js to generate random passwords

---
+++ web/js/gen_pwd.js
@@ -0,0 +1,44 @@
+const button_gen_pwd = document.querySelector("#gen_pwd");
+const gen_pwd_out = document.querySelector('#gen_pwd_out');
+
+// const randonFunc = {
+//     lower: getRandomLower,
+//     upper: getRandomUpper,
+//     number: getRandomNumber,
+//     symbol: getRandomSymbol
+// };
+
+function genRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function genRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function genRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function genRandomSymbol(){
+    var symbols = '$%&#[_?@';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassworwd(){
+    let passLength = 10;
+    let typesCount = 2;
+    let pass = '';
+    for(let i = 0; i < passLength; i += typesCount){
+        pass += 
+        genRandomLower() + 
+        genRandomUpper()
+    }
+    return pass;
+}
+
+button_gen_pwd.addEventListener('click', (e) => {
+    if(gen_pwd_out.tagName == 'INPUT'){
+        gen_pwd_out.value = generatePassworwd();
+    }else{
+        gen_pwd_out.innerText = generatePassworwd();
+    }    
+    e.preventDefault();
+});
