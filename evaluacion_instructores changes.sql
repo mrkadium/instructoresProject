ee7707a1d20804861f6c20846db54f7cefec7a92
Mario Rivera Olivo <mario_rivera.olivo@outlook.es>
2020-05-22 06:12:17 -0600

    Added 'DROP TRIGGER bi_grupo' statement

--- evaluacion_instructores changes.sql
+++ evaluacion_instructores changes.sql
@@ -1,3 +1,6 @@
 -- Agregando opción 'Finalizado' al campo 'estado'
 ALTER TABLE evaluacion_instructores.grupo 
 MODIFY estado ENUM('Habilitado','Inhabilitado','Finalizado') NOT NULL DEFAULT 'Habilitado';
+
+-- Borrando trigger para no generar automáticamente la clave
+DROP TRIGGER bi_grupo;
