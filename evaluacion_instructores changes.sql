0162ae689aa66fcd95205cb8e294f0966e78cb11
Mario Rivera Olivo <mario_rivera.olivo@outlook.es>
2020-05-29 23:35:30 -0600

    Aplied changes to db and updated .war file on server

--- evaluacion_instructores changes.sql
+++ evaluacion_instructores changes.sql
@@ -1,6 +1,9 @@
+-- LOS QUE YA ESTÉN COMENTADOS YA ESTÁN APLICADOS
+/*
 -- Agregando opción 'Finalizado' al campo 'estado'
 ALTER TABLE evaluacion_instructores.grupo 
 MODIFY estado ENUM('Habilitado','Inhabilitado','Finalizado') NOT NULL DEFAULT 'Habilitado';
 
 -- Borrando trigger para no generar automáticamente la clave
 DROP TRIGGER bi_grupo;
+*/
