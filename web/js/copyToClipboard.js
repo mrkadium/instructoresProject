e12cd87ab3f8cfd84b8b3893a04518c977adf05e
Mario Olivo <mario_rivera.olivo@outlook.es>
2020-09-22 13:14:19 -0600

    Fixed some bugs on toast and copy-to-clipboard features

--- web/js/copyToClipboard.js
+++ web/js/copyToClipboard.js
@@ -7,7 +7,7 @@
 function copyToClipboard(e, column_index, link){
     let col = e.target.closest('tr').cells[column_index];
     col.innerHTML +=
-    `<input type='text' style='margin:0;padding:0;border:0;width:0;font-size:1px;' value='${link}${col.textContent}' />`;            
+    `<input type='text' style='' value='${link}${col.textContent}' />`;    
     let text = col.firstElementChild;
     copy(text, true);
     buildToast('Link copiado a portapapeles', 3000);
